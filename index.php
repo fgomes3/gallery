885eed496c9d7c7d9b17dd093df77408fda37286
Fabio Gomes <fabio.gomes@intranet.gazin.com.br>
2016-10-31 13:38:23 -0200

    galeria fotos

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Eventos Gazin</title>
+        <link rel="shortcut icon" href="./assets/img/shorticon.png" type="image/x-icon" />
+        <meta name="viewport"  content="width=device-width, initial-scale=1.0, user-scalable=0, maximum-scale=1.0" />
+        <link rel="stylesheet" href="assets/css/styles.css" />
+        <link rel="stylesheet" href="assets/touchTouch/touchTouch.css" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Dancing+Script" />
+        <!--[if lt IE 9]>
+          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <header>
+            <img src="./assets/img/logogaz.jpg">
+            <h1>Eventos Gazin</h1>
+            <h2>Sempre fazendo o melhor pra você!</h2>
+        </header>
+
+        <div class="thumbs">
+            <a href="http://farm8.staticflickr.com/7013/6754656011_3de2cc73a2_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7013/6754656011_3de2cc73a2_z.jpg)" title="Lion Rock"></a>
+            <a href="http://farm8.staticflickr.com/7042/6895252645_45f5dfffaa_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7042/6895252645_45f5dfffaa_z.jpg)" title="Holsten Gate"></a>
+            <a href="http://farm8.staticflickr.com/7183/6943277737_21b521659c_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7183/6943277737_21b521659c_z.jpg)" title="Blue Hour"></a>
+            <a href="http://farm8.staticflickr.com/7047/7000951429_5eae078a62_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7047/7000951429_5eae078a62_z.jpg)" title="Waikato Landscape"></a>
+            <a href="http://farm6.staticflickr.com/5346/7051537899_efc7a44830_z.jpg" style="background-image:url(http://farm6.staticflickr.com/5346/7051537899_efc7a44830_z.jpg)" title="Tauranga Bridge"></a>
+            <a href="http://farm8.staticflickr.com/7268/6951148260_440661b4d1_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7268/6951148260_440661b4d1_z.jpg)" title="East Coast"></a>
+            <a href="http://farm8.staticflickr.com/7259/6930112984_5fcc076288_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7259/6930112984_5fcc076288_z.jpg)" title="Cathedral Cove"></a>
+            <a href="http://farm8.staticflickr.com/7276/6886626710_047cd03acb_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7276/6886626710_047cd03acb_z.jpg)" title="The Pond"></a>
+            <a href="http://farm8.staticflickr.com/7020/6683299491_f2b5f6aa8b_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7020/6683299491_f2b5f6aa8b_z.jpg)" title="Good Night"></a>
+            <a href="http://farm8.staticflickr.com/7013/6754656011_3de2cc73a2_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7013/6754656011_3de2cc73a2_z.jpg)" title="Lion Rock"></a>
+            <a href="http://farm8.staticflickr.com/7042/6895252645_45f5dfffaa_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7042/6895252645_45f5dfffaa_z.jpg)" title="Holsten Gate"></a>
+            <a href="http://farm8.staticflickr.com/7183/6943277737_21b521659c_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7183/6943277737_21b521659c_z.jpg)" title="Blue Hour"></a>
+            <a href="http://farm8.staticflickr.com/7047/7000951429_5eae078a62_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7047/7000951429_5eae078a62_z.jpg)" title="Waikato Landscape"></a>
+            <a href="http://farm6.staticflickr.com/5346/7051537899_efc7a44830_z.jpg" style="background-image:url(http://farm6.staticflickr.com/5346/7051537899_efc7a44830_z.jpg)" title="Tauranga Bridge"></a>
+            <a href="http://farm8.staticflickr.com/7268/6951148260_440661b4d1_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7268/6951148260_440661b4d1_z.jpg)" title="East Coast"></a>
+            <a href="http://farm8.staticflickr.com/7259/6930112984_5fcc076288_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7259/6930112984_5fcc076288_z.jpg)" title="Cathedral Cove"></a>
+            <a href="http://farm8.staticflickr.com/7276/6886626710_047cd03acb_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7276/6886626710_047cd03acb_z.jpg)" title="The Pond"></a>
+            <a href="http://farm8.staticflickr.com/7020/6683299491_f2b5f6aa8b_z.jpg" style="background-image:url(http://farm8.staticflickr.com/7020/6683299491_f2b5f6aa8b_z.jpg)" title="Good Night"></a>
+
+
+        </div>
+
+        <footer>
+            <div class="container">
+                <div class="rodape"><img src="./assets/img/local-icon.png"> IP: 172.16.22.16</div>
+                <div class="rodape"><img src="./assets/img/calendar-icon.png"><?php echo ' ' . date('d/m/Y') ?></div>
+                <div class="rodape" style="float: right;margin-top: -26px;">© Gazin T.I Desenvolvimento</div>
+            </div>
+        </footer>
+
+        <script src="assets/js/jquery-1.7.1.min.js"></script>
+        <script src="assets/touchTouch/touchTouch.jquery.js"></script>
+        <script src="assets/js/script.js"></script>
+
+    </body>
+</html>
